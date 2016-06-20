df7824f8e8ad95cdcd4551139d8d4b2e4162b1d1
Filippo Ciceri <filippo.ciceri@mail.polimi.it>
2016-06-20 14:42:54 +0200

    device page filler

--- device.php
+++ device.php
@@ -77,19 +77,19 @@
 			<div class="col-xs-12">
 				<div class="tab-content" style="height:718px;">
 						<div class="tab-pane active" id="pres">
-							<div class="show-content">
+							<div class="show-content" style="width: 100%;">
 								<span class="btn btn-primary"><?php echo $lang['SHOW_PROM_BTN']; ?></span>
 							</div>
 						</div>
 						<div class="tab-pane" id="tech">
-							<div class="show-content">
+							<div class="show-content" style="width: 100%;">
 								<span class="btn btn-primary"><?php echo $lang['SHOW_DEV_BTN']; ?></span>
 							</div>
 						</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-xs-12" id="desc">
+		<div class="col-xs-12" id="desc" style="width: 80%; margin-left:10%">
 		</div>
 		<div class="col-xs-12 col-sm-6" id="slRel">
 		</div>
