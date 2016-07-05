c3cd6b84736d8537a32dbbc71d1b322b4ab3ab9e
Federico Cesaro <federico.cesaro@mail.polimi.it>
2016-07-05 16:28:50 +0200

    eng lang

--- assets/languages/lang.en.php
+++ assets/languages/lang.en.php
@@ -1,81 +1,175 @@
 <?php
     /*
-    ------------------
+    -----------------
     Language: English
-    ------------------
+    -----------------
     */
 
     $lang = array();
 
-    $lang['PAGE_TITLE'] = 'TIM';
+    /*HOMEPAGE*/
+    $lang['PAGE_TITLE'] = "TIM";
 
-    $lang['NAV_1'] = "Promotions";
+	$lang['NAV_1'] = "Promotions";
     $lang['NAV_2'] = "Devices";
     $lang['NAV_3'] = "Smart Life";
     $lang['NAV_4'] = "Assistance";
 	
 	$lang['CAROUSEL_HEADER1'] = "TIM SMART HOME";
     $lang['CAROUSEL_DESC1'] = "20 Mb Internet and unlimited calls. <br> Smart Modem Wi-Fi included";
-    $lang['CAROUSEL_BTN1'] = "Scopri l'offerta";
-    $lang['CAROUSEL_HEADER2'] = "TIM SMART HOME";
-    $lang['CAROUSEL_DESC2'] = "20 Mb Internet and unlimited calls. <br> Smart Modem Wi-Fi included";
-    $lang['CAROUSEL_BTN2'] = "Scopri l'offerta";
-    $lang['CAROUSEL_HEADER3'] = "TIM SMART HOME";
-    $lang['CAROUSEL_DESC3'] = "20 Mb Internet and unlimited calls. <br> Smart Modem Wi-Fi included";;
-    $lang['CAROUSEL_BTN3'] = "Scopri l'offerta";
+    $lang['CAROUSEL_BTN1'] = "View deatils";
+    $lang['CAROUSEL_HEADER2'] = "TIM Young&Music <br> Full Digital";
+    $lang['CAROUSEL_DESC2'] = "Summer edition with 3Gb extra!";
+    $lang['CAROUSEL_BTN2'] = "View details";
+    $lang['CAROUSEL_HEADER3'] = "TIM&NETFLIX";
+    $lang['CAROUSEL_DESC3'] = "Super-fast Internet <br> A year of free subscribing";
+    $lang['CAROUSEL_BTN3'] = "View details";
 
 
-    $lang['SHOW_PROMOTION'] = "Scopri l'offerta";
-    $lang['SHOW_DEVICE'] = "Scopri l'offerta";
-    $lang['SHOW_SL'] = "Scopri l'offerta";
+    $lang['SHOW_PROMOTION'] = "Promotions";
+    $lang['SHOW_DEVICE'] = "Devices";
+    $lang['SHOW_SL'] = "Smart Life";
 
-    $lang['SHOW_PROM_HEAD'] = "Scopri l'offerta";
-    $lang['SHOW_PROM_DESC'] = "Scopri l'offerta";
-    $lang['SHOW_PROM_BTN'] = "Scopri l'offerta";
-    $lang['SHOW_DEV_HEAD'] = "Scopri l'offerta";
-    $lang['SHOW_DEV_DESC'] = "Scopri l'offerta";
-    $lang['SHOW_DEV_BTN'] = "Scopri l'offerta";
-    $lang['SHOW_SL_HEAD'] = "Scopri l'offerta";
-    $lang['SHOW_SL_DESC'] = "Scopri l'offerta";
-    $lang['SHOW_SL_BTN'] = "Scopri l'offerta";
+    $lang['SHOW_PROM_HEAD'] = "TIM Young Junior";
+    $lang['SHOW_PROM_DESC'] = "Only 8 €/4 week your son talks and plays with TIMgames Junior and he's safe with TIM Protect.";
+    $lang['SHOW_PROM_BTN'] = "View details";
+    $lang['SHOW_DEV_HEAD'] = "Samsung Galaxy S7 edge";
+    $lang['SHOW_DEV_DESC'] = "More than a smartphone. <br> Only 749,90 € instead of 829,90 € in 36 payments of 20,83 €/month if you have a Tim home line.";
+    $lang['SHOW_DEV_BTN'] = "View details";
+    $lang['SHOW_SL_HEAD'] = "TIM Home Connect";
+    $lang['SHOW_SL_DESC'] = "The best solution to handle all the domotic systems in your house. From the simplest boiler from the most complex security system.";
+    $lang['SHOW_SL_BTN'] = "View details";
 
-    $lang['TOPUP_HEAD'] = "Ricarica";
-    $lang['TOPUP_BTN'] = "Ricarica subito";
+    $lang['TOPUP_HEAD'] = "Recharge";
+    $lang['TOPUP_BTN'] = "recharge now";
     $lang['NEWCUST_HEAD'] = "Join TIM";
-	$lang['NEWCUST_BODY'] = "con l'aiuto di un operatore";
+	$lang['NEWCUST_BODY'] = "ask an operator";
     $lang['NEWCUST_BTN'] = "Call me back";
 
 
-	$lang['FOOTER_LEFT'] = "Come pagare il conto <br>
-							Trasloco <br>
-							Trasparenza tariffaria <br>
+	$lang['FOOTER_LEFT'] = "Pay the bill <br>
+							Moving <br>
+							Tariff transparency <br>
 							Phishing <br>
-							Carta dei Servizi <br>
+							Service card <br>
 							119 SMS <br>
 							ViviTIM <br>
 							News";
-	$lang['FOOTER_CENTER'] = "Ricarica Online <br>
-							  Mail access <br>
-							  Apri una segnalazione <br>
-							  Verifica Copertura <br>
-							  Find Store <br>
-							  Trova Telefono Pubblico <br>
+	$lang['FOOTER_CENTER'] = "Recharge Online <br>
+							  My Mail <br>
+							  Reporting <br>
+							  Verify coverage <br>
+							  Find store <br>
+							  Find public phone <br>
 							  Brochure <br>
-							  Aiutaci a migliorare";
+							  Help us get better";
 	$lang['FOOTER_BOTTOM'] = "Privacy &nbsp &nbsp
-							  Note legali &nbsp&nbsp
+							  Legal notes &nbsp&nbsp
 							  Website &nbsp&nbsp
-							  Info Contatti &nbsp&nbsp
-							  Per i Consumatori  &nbsp&nbsp
-							  Tutela Minori &nbsp&nbsp
-							  Lavora con noi  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+							  For Costumers  &nbsp&nbsp
+							  Child protection &nbsp&nbsp
+							  Work with us  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
 							  Telecom Italia 2012 - P.IVA 00488410010";
-	$lang['FOOTER_SOCIAL'] = "Scopri l'offerta";
-	$lang['FOOTER_BTN1'] = "Scopri l'offerta";
-	$lang['FOOTER_BTN2'] = "Scopri l'offerta";
-	$lang['USEFUL_INFO'] = "INFORMAZIONI UTILI";
-	$lang['QUICK_ACCESS'] = "STRUMENTI VELOCI";
-	$lang['DIGITAL_ID'] = "Identità digitale";
-	$lang['THE_GROUP_FOOTER'] = "Gruppo Telecom Italia";
+	$lang['FOOTER_SOCIAL'] = "Follow us";
+	$lang['FOOTER_BTN1'] = "Who we are";
+	$lang['FOOTER_BTN2'] = "The group";
+	$lang['USEFUL_INFO'] = "USEFUL INFORMATION";
+	$lang['QUICK_ACCESS'] = "QUICK ACCESS";
+	$lang['DIGITAL_ID'] = "Digital Identity";
+	$lang['THE_GROUP_FOOTER'] = "Telecom Italia Group";
 
+    /*DEVICES*/
+    $lang['DEVICES_PAGE_TITLE'] = "Devices | TIM";
+
+    $lang['PRODS_BANNER_TITLE'] = "Devices";
+    $lang['PRODS_BANNER_DESC'] = "Smartphone, Tablet, phones for you and your house";
+
+    $lang['SPS_FILTER_TITLE'] = "Smartphones";
+    $lang['SPS_FILTER_ALL'] = "All";
+    $lang['SPS_FILTER_APPLE'] = "Apple";
+    $lang['SPS_FILTER_SAMSUNG'] = "Samsung";
+    $lang['TBS_FILTER_TITLE'] = "Tablet";
+    $lang['TBS_FILTER_ALL'] = "All";
+    $lang['TBS_FILTER_APPLE'] = "Apple";
+    $lang['TBS_FILTER_SAMSUNG'] = "Samsung";
+    $lang['MODS_FILTER_TITLE'] = "Modem & Networking";
+    $lang['MODS_FILTER_ALL'] = "All";
+    $lang['MODS_FILTER_APPLE'] = "Apple";
+    $lang['MODS_FILTER_SAMSUNG'] = "Samsung";
+    $lang['TVSLS_FILTER_TITLE'] = "TV & Smart Living";
+    $lang['TVSLS_FILTER_ALL'] = "All";
+    $lang['TVSLS_FILTER_APPLE'] = "Apple";
+    $lang['TVSLS_FILTER_SAMSUNG'] = "Samsung";
+
+    /*PROMOTIONS*/
+    $lang['OFF_PAGE_TITLE'] = "Promotions | TIM";
+
+    $lang['OFF_BANNER_TITLE'] = "TIM offers";
+    $lang['OFF_BANNER_DESC'] = "Best offers for you and your house";
+
+    $lang['PROM_DEV_TITLE'] = "Smartphone e tablet";
+    $lang['PROM_DEV_ALL'] = "All";
+    $lang['PROM_DEV_SMART'] = "Smartphone";
+    $lang['PROM_DEV_TAB'] = "Tablet";
+    $lang['PROM_DEV_APPLE'] = "Apple";
+    $lang['PROM_DEV_SAMS'] = "Samsung";
+    $lang['PROM_SHOW_SMART'] = "Choose your smartphone";
+    $lang['PROM_SHOW_SMART2'] = "The best for you";
+    $lang['PROM_SHOW_TAB'] = "Choose your tablet";
+    $lang['PROM_SHOW_TAB2'] = "With the best offer";
+
+    $lang['PROM_SWITCH_ALL'] = "All";
+    $lang['PROM_SWITCH_OFFERS'] = "Discount";
+
+    $lang['PROM_SL_TITLE'] = "TV & Entertainment";
+    $lang['PROM_SL1'] = "BIG HELLO";
+    $lang['PROM_SL2'] = "BIG HELLO";
+    $lang['PROM_SL3'] = "BIG HELLO";
+    $lang['PROM_SL4'] = "BIG HELLO";
+    $lang['PROM_SHOW_SL1'] = "Tim Sky";
+    $lang['PROM_SHOW_SL1_2'] = "Calls, Internet and Sky";
+    $lang['PROM_SHOW_SL2'] = "Tim Vision";
+    $lang['PROM_SHOW_SL2_2'] = "Movies, Tv Series, cartoons, whenever and wherever you want";
+
+    /*DEVICES*/
+    $lang['DEVICES_PAGE_TITLE'] = "Devices  | TIM";
+
+    $lang['PRODS_BANNER_TITLE'] = "Devices";
+    $lang['PRODS_BANNER_DESC'] = "Smartphone, Tablet, phones for you and your house";
+
+    /*SL SERVICES*/
+    $lang['SL_PAGE_TITLE'] = "Smart Life | TIM";
+
+    $lang['SL_TV_TITLE'] = "TV & Entertainment";
+    $lang['SL_TV_DESC'] = "TV SERIES, MOVIES, MUSIC, BOOKS, GAMES AND FOOTBALL. <br>Discover the best entertainment for you. It starts now!<br>Whenever and wherever you want.";
+    $lang['SL_HEALTH_TITLE'] = "Health & Wellness";
+    $lang['SL_HEALTH_DESC'] = "Try the latest wearables. Find out how health and wellness can positively change your life. <br> Free your desire to move. Share your results with friends. The future is smart.";
+    $lang['SL_FAMILY_TITLE'] = "Home & Family";
+    $lang['SL_FAMILY_DESC'] = "Smart Life means simpler and safer life.<br>Also for you home and your children.";
+    $lang['SL_PERSONAL_TITLE'] = "Personal Services";
+    $lang['SL_PERSONAL_DESC'] = "A new approach to the world of e-payment and digital identity.<br>You'll be able to organize and manage easily your digital payments,<br>keep passwords, invoices , receipts and medical reports.";
+    $lang['BTN_FINDOUT'] = "View details";
+
+    $lang['SL_BREAD_CAT1'] = "TV %26 Entertainment";
+    $lang['SL_BREAD_CAT2'] = "Health %26 Wellness";
+    $lang['SL_BREAD_CAT3'] = "Home %26 Family";
+    $lang['SL_BREAD_CAT4'] = "Personal Services";
+
+    /*ASSISTANCE SERVICES*/
+    $lang['ASS_PAGE_TITLE'] = "Assistance Services | TIM";
+
+    $lang['ASS_BANNER_TITLE'] = "BIG HELLO";
+    $lang['ASS_BANNER_DESC'] = "BIG HELLO";
+
+    /*DEVICE*/
+    $lang['DEVICE_PAGE_TITLE'] = "Device | TIM";
+
+    $lang['DEV_PRES'] = "Presentation";
+    $lang['DEV_SPEC'] = "Technical specifications";
+    $lang['BUY_BTN'] = "Buy";
+
+    /*BREADCRUMB*/
+    $lang['BREAD_PROD'] = "Devices";
+    $lang['BREAD_PROM'] = "Promotions";
+    $lang['BREAD_SL'] = "Smart Life Services";
 ?>
